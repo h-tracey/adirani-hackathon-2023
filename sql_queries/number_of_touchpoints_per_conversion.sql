f2da8fb5494f422f2e93bf8fa8e6c41cb7b52637
Henry Tracey <henry@relativeinsight.com>
2023-02-18 15:15:24 +0000

    sql style points

--- sql_queries/number_of_touchpoints_per_conversion.sql
+++ sql_queries/number_of_touchpoints_per_conversion.sql
@@ -1,6 +1,6 @@
 SELECT 
     JSON_LENGTH(`Default channel group`) AS num_touchpoints,
-    sum(Conversions)
+    sum(Conversions) as num_conversions
 FROM
     analytics.advertising_overview_touchpoints_to_conversion
-group by array_length
+group by num_touchpoints
--- sql_queries/total_conversions_per_channel.sql
+++ sql_queries/total_conversions_per_channel.sql
@@ -1,4 +1,5 @@
 SELECT 
-    *
+    `Default channel group` as default_channel_group,
+	`Conversions` as conversions
 FROM
     analytics.advertising_overview_channels_driving_conversions;
